0d42d1a87c6e7125a467f62aa2d83c443af00408
Sphesihle Jamile <jamilesphesihle99@gmail.com>
2022-10-25 00:22:03 +0200

    Bussiness Details Step 1 Interface Complete

---
+++ app/src/main/java/com/example/onlineshoppingstore/Business_Details_Step_1.java
@@ -0,0 +1,14 @@
+package com.example.onlineshoppingstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Business_Details_Step_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_details_step1);
+    }
+}
